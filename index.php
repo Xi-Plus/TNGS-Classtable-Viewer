11b1616e8bf0500d2b84d7fd76f5c1d1525c186b
xiplus <huangxuanyuxiplus@gmail.com>
2016-06-30 20:37:36 +0800

    End of Support

--- index.php
+++ index.php
@@ -2,90 +2,14 @@
 <head>
 	<meta charset="UTF-8">
 	<title>台南女中課表查詢系統 for Google Chrome</title>
-	<meta name=viewport content="width=700, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="refresh" content="15; url=http://w3.tngs.tn.edu.tw/departments/teach/classtable/">
 </head>
 <body bgColor="#ccccff" background="http://w3.tngs.tn.edu.tw/departments/teach/classtable/images/bg.jpg" marginheight="">
 <center>
 <h2>台南女中課表查詢系統 for Google Chrome</h2>
-若你現在使用的是 <b>Internet Explorer 8</b> 或是<b>更舊版本</b>，建議你使用<a href="http://w3.tngs.tn.edu.tw/departments/teach/classtable/">正版網站</a><br><br>
-<table border="0" cellspacing="0" cellpadding="0">
-<tr>
-<td align="center">
-
-<form action="" method="get">
-<input name="type" type="hidden" value="class">
-<?php
-$html=@file_get_contents("http://w3.tngs.tn.edu.tw/departments/teach/classtable/top.asp");
-$html=str_replace("\r\n", "", $html);
-$html=iconv("BIG5", "UTF-8//IGNORE", $html);
-preg_match('(<select name="select1".*?<\/select>)', $html, $match);
-$temp=$match[0];
-$temp=preg_replace('/style=".*?"/', "", $temp);
-$temp=preg_replace('/onChange=".*?"/', "", $temp);
-$temp=preg_replace('/onFocus=".*?"/', "", $temp);
-$temp=preg_replace('/name=".*?"/', 'name="sqlstr"', $temp);
-echo $temp;
-?>
-<input name="" type="submit" value="submit">
-</form>
-
-</td><td>
-
-<form action="" method="get">
-<input name="type" type="hidden" value="teacher">
-<?php
-preg_match('(<select id="s2" name="select2".*?<\/select>)', $html, $match);
-$temp=$match[0];
-$temp=preg_replace('/style=".*?"/', "", $temp);
-$temp=preg_replace('/onChange=".*?"/', "", $temp);
-$temp=preg_replace('/onFocus=".*?"/', "", $temp);
-$temp=preg_replace('/name=".*?"/', 'name="sqlstr"', $temp);
-echo $temp;
-?>
-<input name="" type="submit" value="submit">
-</form>
-
-</td><td>
-
-<form action="" method="get">
-<input name="type" type="hidden" value="room">
-<?php
-preg_match('(<SELECT id="s3" name="Select3".*?<\/SELECT>)', $html, $match);
-$temp=$match[0];
-$temp=preg_replace('/style=".*?"/', "", $temp);
-$temp=preg_replace('/onChange=".*?"/', "", $temp);
-$temp=preg_replace('/onFocus=".*?"/', "", $temp);
-$temp=preg_replace('/name=".*?"/', 'name="sqlstr"', $temp);
-echo $temp;
-?>
-<input name="" type="submit" value="submit">
-</form>
-
-</td>
-</tr>
-</table>
-<?php
-if(isset($_GET["sqlstr"])){
-	$url="http://w3.tngs.tn.edu.tw/departments/teach/classtable/down.asp?sqlstr=".urlencode($_GET["sqlstr"])."&type=".urlencode($_GET["type"]);
-	$html=@file_get_contents($url);
-	$html=iconv("BIG5", "UTF-8//IGNORE", $html);
-	$html=str_replace("down.asp", "", $html);
-	$html=str_replace("images/bg.jpg", "http://w3.tngs.tn.edu.tw/departments/teach/classtable/images/bg.jpg", $html);
-	echo $html;
-	?>
-<script>
-<?php 
-	$typelist=array(
-		"class"=>1,
-		"teacher"=>2,
-		"room"=>3
-	);
-	echo 's'.$typelist[$_GET["type"]].'.value="'.urldecode($_GET["sqlstr"]).'"';
-?>
-</script>
-<?php
-}
-?>
+由於臺南女中課表已更新版本，能夠於Google Chrome及手機瀏覽器正常顯示，所以此專案已不再必要也無法運作，決定此專案將不再更新，真是可喜可賀。<br>
+即將為你重新導向至正版網站，或是<a href="http://w3.tngs.tn.edu.tw/departments/teach/classtable/">點我立即前往</a><br><br>
 <hr>
 <?php
 @include("function/Xiplus-Facebook-Badge/badge.php");
